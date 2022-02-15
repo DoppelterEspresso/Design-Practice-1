f731f687d0158cf66788d2656faee76d5cceda1a
Edwin <edwin.kaisers0451@gmail.com>
2022-02-15 22:34:13 +0100

    First working version

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Stoffie1 from "./images/Stoffie1.jpg";
+import Stoffie2 from "./images/Stoffie2.jpg";
+import Stoffie3 from "./images/Stoffie3.jpg";
+
+document.getElementsByTagName("img")[0].src = Stoffie1;
+document.getElementsByTagName("img")[1].src = Stoffie2;
+document.getElementsByTagName("img")[2].src = Stoffie3;
